60a08d6255697d65dc071cfda7cce6b6249f1aa5
GRAS David <david.gras@dr11.cnrs.fr>
2020-05-15 19:58:39 +0200

    fichier swmb "local" ou "my"

---
+++ Win10-My-swmb.ps1
@@ -0,0 +1,17 @@
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+$ScriptDir += "\swmb-var-local.psm1"
+Import-Module -name $ScriptDir -verbose
+
+#Renommage du compte administrateur
+#Configuration ordinateur/Paramètres Windows/Paramètres de sécurité/stratégies locales/Options de sécurité
+#Enable
+EnableRenameAdminAccount {
+   $localAdminName = get-localuser |  where-object {($_.SID -like "S-1-5-21*-500")}
+   Rename-LocalUser -Name $localAdminName.name -NewName $localAdminNameToSet -ErrorAction Continue
+}
+
+#Disable
+DisableRenameAdminAccount {
+   $localAdminName = get-localuser |  where-object {($_.SID -like "S-1-5-21*-500")}
+   Rename-LocalUser -Name $localAdminName.name -NewName $administrateur -ErrorAction Continue
+}

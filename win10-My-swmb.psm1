6836f586a05e8f03afa86b488a3d2b220f9ce2b8
GRAS David <david.gras@dr11.cnrs.fr>
2020-05-15 23:53:36 +0200

    Delete win10-My-swmb.psm1

--- win10-My-swmb.psm1
+++
@@ -1,7 +0,0 @@
-$localAdminNameToSet = "sas-swmb"
-$localAdminNameOriginal = "administrateur"
-$InactivityTimeoutSecs = 1200
-
-Export-ModuleMember -Variable 'localAdminNameToSet'
-Export-ModuleMember -Variable 'localAdminNameOriginal'
-Export-ModuleMember -Variable 'InactivityTimeoutSecs'

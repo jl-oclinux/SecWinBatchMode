4b91cf5e7c68233b25f782509fc4df073478c48b
GRAS David <david.gras@dr11.cnrs.fr>
2020-05-15 23:25:20 +0200

    rajout de EnableDontDisplayLastUsername

--- Win10-My-swmb.ps1
+++ Win10-My-swmb.ps1
@@ -20,11 +20,49 @@ DisableRenameAdminAccount {
 # Enable
 Function EnableDontDisplayLastUsername {
 	Write-Output "Ne pas afficher le dernier utilisateur..."
+	If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System")) {
+		New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Force | Out-Null
+	}
 	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "dontdisplaylastusername" -Type DWord -Value 1 -ErrorAction SilentlyContinue
 }
 
 # Disable
 Function DisableDontDisplayLastUsername {
 	Write-Output "Afficher le dernier utilisateur..."
+    If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System")) {
+		New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Force | Out-Null
+	}
 	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "dontdisplaylastusername" -Type DWord -Value 0 -ErrorAction SilentlyContinue
+}
+
+#verrouillage de la session : timeout de session
+# Enable
+Function EnableSessionLock {
+	Write-Output "Définition du timeout de session..."
+	If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System")) {
+		New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "dontdisplaylastusername" -Type DWord -Value 1 -ErrorAction SilentlyContinue
+}
+
+# Disable
+Function DisableSessionLock{
+	Write-Output "Définition du timeout de session..."
+    If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System")) {
+		New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "dontdisplaylastusername" -Type DWord -Value 0 -ErrorAction SilentlyContinue
+}
+
+#Ne pas afficher le nom du dernier utilisateur
+# Enable
+Function EnableDontDisplayLastUsername {
+	Write-Output "Ne pas afficher le dernier utilisateur..."
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "InactivityTimeoutSecs" -Type DWord -Value $localSessionTimeout -ErrorAction SilentlyContinue
+}
+
+# Disable
+Function DisableDontDisplayLastUsername {
+	Write-Output "Afficher le dernier utilisateur..."
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "InactivityTimeoutSecs" -Type DWord -Value 0
 }

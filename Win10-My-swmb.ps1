41417f5b0a3122362b1185c5d05b7b60e694fd36
GRAS David <david.gras@dr11.cnrs.fr>
2020-05-15 23:46:05 +0200

    Rajout du tiemout de session

--- Win10-My-swmb.ps1
+++ Win10-My-swmb.ps1
@@ -1,22 +1,22 @@
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 $ScriptDir += "\win10-My-swmb.psm1"
-Import-Module -name $ScriptDir -verbose
+Import-Module -name $ScriptDir
 
 #Renommage du compte administrateur
 #Configuration ordinateur/Paramètres Windows/Paramètres de sécurité/stratégies locales/Options de sécurité
 #Enable
-EnableRenameAdminAccount {
+Function EnableRenameAdminAccount {
    $localAdminName = get-localuser |  where-object {($_.SID -like "S-1-5-21*-500")}
    Rename-LocalUser -Name $localAdminName.name -NewName $localAdminNameToSet -ErrorAction SilentlyContinue
 }
 
 #Disable
-DisableRenameAdminAccount {
+Function DisableRenameAdminAccount {
    $localAdminName = get-localuser |  where-object {($_.SID -like "S-1-5-21*-500")}
    Rename-LocalUser -Name $localAdminName.name -NewName $administrateur -ErrorAction SilentlyContinue
 }
 
-#ne pas afficher le nom du dernier utilisateur
+#Ne pas afficher le nom du dernier utilisateur
 # Enable
 Function EnableDontDisplayLastUsername {
 	Write-Output "Ne pas afficher le dernier utilisateur..."
@@ -37,32 +37,19 @@ Function DisableDontDisplayLastUsername {
 
 #verrouillage de la session : timeout de session
 # Enable
-Function EnableSessionLock {
+Function EnableSessionLockTimeout {
 	Write-Output "Définition du timeout de session..."
 	If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System")) {
 		New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Force | Out-Null
 	}
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "dontdisplaylastusername" -Type DWord -Value 1 -ErrorAction SilentlyContinue
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "InactivityTimeoutSecs" -Type DWord -Value $InactivityTimeoutSecs -ErrorAction SilentlyContinue
 }
 
 # Disable
-Function DisableSessionLock{
-	Write-Output "Définition du timeout de session..."
+Function DisableSessionLockTimeout {
+	Write-Output "Suppression du timeout de session..."
     If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System")) {
 		New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Force | Out-Null
 	}
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "dontdisplaylastusername" -Type DWord -Value 0 -ErrorAction SilentlyContinue
-}
-
-#Ne pas afficher le nom du dernier utilisateur
-# Enable
-Function EnableDontDisplayLastUsername {
-	Write-Output "Ne pas afficher le dernier utilisateur..."
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "InactivityTimeoutSecs" -Type DWord -Value $localSessionTimeout -ErrorAction SilentlyContinue
-}
-
-# Disable
-Function DisableDontDisplayLastUsername {
-	Write-Output "Afficher le dernier utilisateur..."
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "InactivityTimeoutSecs" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "InactivityTimeoutSecs" -Type DWord -Value 0 -ErrorAction SilentlyContinue
 }

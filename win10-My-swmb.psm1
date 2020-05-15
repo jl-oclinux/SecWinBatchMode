36f3bba8cc853e429f0cce8c8ced3a6fbd2d4fa1
GRAS David <david.gras@dr11.cnrs.fr>
2020-05-15 23:26:11 +0200

    Rajout de localSessionTimeout

--- win10-My-swmb.psm1
+++ win10-My-swmb.psm1
@@ -1,5 +1,7 @@
 $localAdminNameToSet = "sas-swmb"
 $localAdminNameOriginal = "administrateur"
+$localSessionTimeout = 20
 
 Export-ModuleMember -Variable 'localAdminNameToSet'
 Export-ModuleMember -Variable 'localAdminNameOriginal'
+Export-ModuleMember -Variable 'localSessionTimeout'

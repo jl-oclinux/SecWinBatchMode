da812f2f0a62b5adf1e81a9c5a4276ec116f867e
Gabriel.Moreau <Gabriel.Moreau@univ-grenoble-alpes.fr>
2020-05-27 18:17:16 +0200

    Update comment and authors

--- Win10-My-Swmb-VarDefault.psm1
+++ Win10-My-Swmb-VarDefault.psm1
@@ -4,10 +4,10 @@
 # License: MIT License (Same as project Win10-Initial-Setup-Script)
 # Homepage: https://gitlab.in2p3.fr/resinfo-gt/swmb
 # Authors:
-#  2020 - Olivier de Marchi (LEGI)
-#  2020 - David Gras (DR11)
-#  2020 - Clément Deiber (DR11)
-#  2020 - Gabriel Moreau (LEGI)
+#  2020 - Olivier de Marchi (Grenoble INP / LEGI)
+#  2020 - David Gras (CNRS / DR11)
+#  2020 - Clément Deiber (CNRS / DR11)
+#  2020 - Gabriel Moreau (CNRS / LEGI)
 ################################################################
 
 # Variables utilisées dans le script Win10-My-Swmb.psm1

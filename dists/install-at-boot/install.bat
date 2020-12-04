a84442a8592842d082fc69b74e6a74fe2c9b7146
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-12-04 18:38:28 +0100

    Reorganize the script (variable name)

--- dists/install-at-boot/install.bat
+++ dists/install-at-boot/install.bat
@@ -8,15 +8,17 @@ REM une tache planifiee qui execute le script au démarrage du poste
 REM
 REM --------------------------------------------------------------------------
 
+REM repertoire des fichiers de script "sources"
+SET srcFolder="\\MyServer\MySecuriseShare"
+
+REM repertoire local d'installation des fichiers des scripts
+SET destFolder="C:\SWMB"
+
+REM log file name
+SET logfile=logfileSWMBInstallation.txt
+
+REM powershell executable
 SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
-
-REM Repertoire des fichiers de script "sources"
-SET repSource="\\Monserver\MonPartageSecurise"
-
-REM Repertoire local des fichiers de script
-SET swmbDirectory="C:\SWMB"
-
-SET logfile=logfileInstllation.txt
 
 
 ECHO %date%-%time% > %~dp0\%logfile%
@@ -25,14 +27,14 @@ REM ajoute temporaire des droits d'exécution
 %pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope Process 2>&1 >> %~dp0\%logfile%
 
 REM creation du répertoire
-IF NOT EXIST %swmbDirectory% (mkdir %swmbDirectory%  2>&1 >> %~dp0\%logfile%)
+IF NOT EXIST %destFolder% (mkdir %destFolder%  2>&1 >> %~dp0\%logfile%)
 
 REM copie des scripts
-XCOPY  %repSource%\*  %swmbDirectory%\ /E /I /Q /R /Y 2>&1 >> %~dp0\%logfile%
+XCOPY  %srcFolder%\*  %destFolder%\ /E /I /Q /R /Y 2>&1 >> %~dp0\%logfile%
 
-REM déblocage des fichiers
-%pwrsh% "dir -Path %swmbDirectory% -recurse | Unblock-File" >> %~dp0\%logfile%
+REM deblocage des fichiers
+%pwrsh% "dir -Path %destFolder% -recurse | Unblock-File" >> %~dp0\%logfile%
 
 
 REM creation de la tache planifiee
-schtasks /create /tn swmbAtBoot /sc onstart /RU SYSTEM  /F /tr "powershell.exe -ExecutionPolicy RemoteSigned -file '%swmbDirectory%\Win10-Initial-Setup-Script\Win10.ps1' -include '%swmbDirectory%\Win10-Initial-Setup-Script\Win10.psm1' -include '%swmbDirectory%\Win10-Resinfo-Swmb.psm1' -preset '%swmbDirectory%\Presets\UserExperience-Resinfo.preset'"  2>&1 >> %~dp0\%logfile%
+schtasks /create /tn swmbAtBoot /sc onstart /RU SYSTEM  /F /tr "powershell.exe -ExecutionPolicy RemoteSigned -file '%destFolder%\Win10-Initial-Setup-Script\Win10.ps1' -include '%destFolder%\Win10-Initial-Setup-Script\Win10.psm1' -include '%destFolder%\Win10-Resinfo-Swmb.psm1' -preset '%destFolder%\Presets\UserExperience-Resinfo.preset'"  2>&1 >> %~dp0\%logfile%

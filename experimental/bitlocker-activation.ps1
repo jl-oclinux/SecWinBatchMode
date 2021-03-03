0a2b08a43756067553c2ddebba01481ae5462e26
david.gras@dr11.cnrs.fr <david.gras@dr11.cnrs.fr>
2021-03-03 12:18:07 +0100

    rajout du chiffrement de tous les drives autres que drive system

--- experimental/bitlocker-activation.ps1
+++ experimental/bitlocker-activation.ps1
@@ -34,6 +34,8 @@ Function EnableBitlocker {
 ## Commandes PowerShell bitlocker
 # https://docs.microsoft.com/en-us/powershell/module/bitlocker/?view=win10-ps
 $systemDrive = $env:systemdrive
+$systemDriveLetter = $systemDrive.substring(0,1)
+$numVolume = 0
 
 if (!(Confirm-SecureBootUEFI)) {
    Write-Error "UEFI is OFF !"
@@ -95,22 +97,63 @@ else {
 }
 
 
-#TODO boucle sur les lecteurs
-If ((Test-Path D:))
-{
-  $bitOnDriveD = Read-Host -Prompt 'Drive D: exist, do you like to activate bitlocker on drive D: ? [yes/no] (default is no)'
-  if ($bitOnDriveD -eq "yes") {
-    Write-Host "Enable bitlocker on D:"
-    Enable-BitLocker -MountPoint "D:" -RecoveryPasswordProtector -EncryptionMethod "XtsAes256"
-    Write-Host "Resume disk d"
-    Resume-BitLocker -MountPoint "D:"
-    Write-Host "Copy key"
-    (Get-BitLockerVolume -MountPoint D).KeyProtector > c:\"$env:computername"-bitlockerRecoveryKey-D.txt
-    ## Auto unlock D mais ne fonctionne pas avant reboot
-    # aprÃ¨s reboot
-    # Unlock-BitLocker -MountPoint "D:" -recoverypassword xxxxx
-    # Enable-BitLockerAutoUnlock -MountPoint "D:"
-  }
-}
+###TODO boucle sur les lecteurs
+###If ((Test-Path D:))
+###{
+###  $bitOnDriveD = Read-Host -Prompt 'Drive D: exist, do you like to activate bitlocker on drive D: ? [yes/no] (default is no)'
+###  if ($bitOnDriveD -eq "yes") {
+###    Write-Host "Enable bitlocker on D:"
+###    Enable-BitLocker -MountPoint "D:" -RecoveryPasswordProtector -EncryptionMethod "XtsAes256"
+###    Write-Host "Resume disk d"
+###    Resume-BitLocker -MountPoint "D:"
+###    Write-Host "Copy key"
+###    (Get-BitLockerVolume -MountPoint D).KeyProtector > c:\"$env:computername"-bitlockerRecoveryKey-D.txt
+###    ## Auto unlock D mais ne fonctionne pas avant reboot
+###    # aprÃ¨s reboot
+###    # Unlock-BitLocker -MountPoint "D:" -recoverypassword xxxxx
+###    # Enable-BitLockerAutoUnlock -MountPoint "D:"
+###  }
+### }
+
+# On traite toutes les partitions qui ont une lettre associée et qui sont de type fixed
+# ie on ne prend pas en compte les clés usb
+
+$List_volume = Get-volume | Where-Object {$_.DriveType -eq "Fixed"  -and $_.DriveLetter -ne $systemDriveLetter }
+$Nb_volume = $List_volume.count
+
+
+do {
+    $volume = $List_volume[$numVolume]
+    if ($volume.DriveLetter) {
+        $Letter = $volume.DriveLetter
+		$LetterColon = $letter + ":"
+        #if (Test-Path $letter){
+        $ChiffDrv = Read-Host -Prompt "The drive $letter is not removable and hosts a file system. Do you want to active Bitlocker on this drive ? [Y/N]"
+        if ($ChiffDrv -eq "Y") {
+            Write-Host "Bitlocker activation on drive $letter is going to start"
+			
+			##TODO 
+			#A voir pourquoi on reteste pas si partition déjà chiffrée (comme pour C:)
+			#A rajouter copie de la clé sur réseau si $networkKeyBackupFolder = true
+			Enable-BitLocker -MountPoint $letter -RecoveryPasswordProtector -EncryptionMethod "XtsAes256"
+			Resume-BitLocker -MountPoint $letter
+			Write-Host "Copy key"
+			$backupFile = $systemDrive + "\" + $env:computername +"-bitlockerRecoveryKey-"+ $Letter + ".txt"
+			write-host $backupFile
+			(Get-BitLockerVolume -MountPoint $LetterColon).KeyProtector > $backupFile
+                    
+            #$NextVolume = Read-Host -Prompt "Voulez vous chiffre un autre lecteur ? [O/N]"
+            #if ($NextVolume -ne 'O'){
+            #Write-Host "Chiffrement BitLocker terminé pour tous les lecteurs"
+            #exit
+            #}
+			Write-Host "Bitlocker activation on drive $letter ended with success"
+        }
+    }
+    #} 
+    $numVolume++
+} until ($numVolume -eq $Nb_volume) 
+
+write-host "Bitlocker script ended with success!"
 
 }

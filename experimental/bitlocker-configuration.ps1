357a58dc1464885da992a7c1c517af3b6f35d1ce
Olivier De Marchi <olivier.de-marchi@grenoble-inp.fr>
2021-03-18 16:28:51 +0100

    comment recovery bitlocker

--- experimental/bitlocker-configuration.ps1
+++ experimental/bitlocker-configuration.ps1
@@ -40,11 +40,19 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "DisallowSt
 
 # méthode de recouvrement autorisée
 # https://admx.help/?Category=Windows_10_2016&Policy=Microsoft.Policies.VolumeEncryption::OSRecoveryUsage_Name
+# Active this GPO
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "OSRecovery" -Value 1
+# Allow data recovery agent
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "OSManageDRA" -Value 1
+# Allow 48-digit recovery password
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "OSRecoveryPassword" -Value 2
+# Allow 256-bit recovery key
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "OSRecoveryKey" -Value 2
+# Omit recovery options from the BitLocker setup wizard
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "OSHideRecoveryPage" -Value 0
+# Don't Save BitLocker recovery information to AD DS for operating system drives
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "OSActiveDirectoryBackup" -Value 0
+# Store recovery passwords and key packages
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "OSActiveDirectoryInfoToStore" -Value 1
+# Do not enable BitLocker until recovery information is stored to AD DS for operating system drives
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "OSRequireActiveDirectoryBackup" -Value 0

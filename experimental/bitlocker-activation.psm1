6c84b443e973acd7ea4a397f7e69122e6f464f18
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2021-05-12 16:59:45 +0200

    Move all function in one file

--- experimental/bitlocker-activation.psm1
+++ experimental/bitlocker-activation.psm1
@@ -273,3 +273,12 @@ Function EnableBitlocker {
 		}
 	}
 }
+
+Function DisableBitlocker {
+	$listVolume = Get-volume | Where-Object { $_.DriveType -eq "Fixed" }
+	foreach ($volume in $listVolume) {
+		if (-not ($volume.DriveLetter)) { continue }
+		$letter = $volume.DriveLetter
+		Disable-BitLocker $letter
+	}
+}

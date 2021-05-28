6eb22227720265d1b5899e23ab8bfe49f45fee7b
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2021-05-28 10:59:03 +0200

    Add méta modules SWMB !

---
+++ Modules/SWMB.psm1
@@ -0,0 +1,14 @@
+################################################################
+# Project CNRS RESINFO SWMB
+# Copyright (C) 2020-2021, CNRS, France
+# License: MIT License (Same as project Win10-Initial-Setup-Script)
+# Homepage: https://gitlab.in2p3.fr/resinfo-gt/swmb
+# Authors:
+#  2020 - Olivier de Marchi (Grenoble INP / LEGI)
+#  2020 - David Gras (CNRS / DR11)
+#  2020 - Clément Deiber (CNRS / DR11)
+#  2020 - Gabriel Moreau (CNRS / LEGI)
+################################################################
+
+using module ..\Win10-Initial-Setup-Script\Win10.psm1
+using module ..\Win10-Resinfo-Swmb.psm1

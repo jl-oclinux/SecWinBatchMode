871df7c87758bfef7d2f6128c0b4d60853266ede
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2021-06-02 17:36:07 +0200

    Just export all function !

--- Modules/SWMB.psm1
+++ Modules/SWMB.psm1
@@ -10,4 +10,49 @@
 #  2020 - Gabriel Moreau (CNRS / LEGI)
 ################################################################
 
-# Empty module
+Function ImportModuleParameter() {
+	Param (
+		[Parameter(Mandatory = $true)] [string]$moduleScriptName
+	)
+
+	$moduleScriptPath = (Get-Item $moduleScriptName).DirectoryName
+	$moduleScriptBasename = (Get-Item $moduleScriptName).Basename
+
+	# Try to load default parameter module with extension -VarDefault
+	$moduleScriptVarDefault = (Join-Path -Path $moduleScriptPath -ChildPath $moduleScriptBasename) + '-VarDefault.psm1'
+	If (Test-Path -LiteralPath $moduleScriptVarDefault) {
+		Write-Host "Debug02 " $moduleScriptVarDefault
+		Import-Module -Name $moduleScriptVarDefault -ErrorAction Stop
+	}
+
+	# Try to load local overload parameter module with extension -VarOverload
+	While (Test-Path -LiteralPath $moduleScriptPath) {
+		# Module VarOverload directly in the current folder
+		$moduleScriptVarOverload1 = (Join-Path -Path $moduleScriptPath -ChildPath $moduleScriptBasename) + '-VarOverload.psm1'
+		If (Test-Path -LiteralPath $moduleScriptVarOverload1) {
+			Import-Module -Name $moduleScriptVarOverload1 -ErrorAction Stop
+			Break
+		}
+		# Or module VarOverload directly in the subfolder Modules
+		$moduleScriptVarOverload2 = (Join-Path $moduleScriptPath "Modules" $moduleScriptBasename) + '-VarOverload.psm1'
+		If (Test-Path -LiteralPath $moduleScriptVarOverload2) {
+			Import-Module -Name $moduleScriptVarOverload2 -ErrorAction Stop
+			Break
+		}
+
+		# Search module in the parent folder .. and so on
+		$newPath = (Resolve-Path (Join-Path $moduleScriptPath "..") -ErrorAction SilentlyContinue) 
+		If ("$newPath" -eq "$moduleScriptPath") {
+			Break
+		}
+		$moduleScriptPath = $newPath
+	}
+}
+
+
+################################################################
+###### Export Functions
+################################################################
+
+# Export functions
+Export-ModuleMember -Function *

4b17dc1d39c3496c2636d863afc8c9a9ae2682d6
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2021-07-10 13:44:47 +0200

    Add script for compatibility issues

---
+++ Win10.ps1
@@ -0,0 +1,19 @@
+################################################################
+# Project CNRS RESINFO SWMB
+# Copyright (C) 2020-2021, CNRS, France
+# License: MIT License (Same as project Win10-Initial-Setup-Script)
+# Homepage: https://gitlab.in2p3.fr/resinfo-gt/swmb
+# Authors:
+#  2021 - Gabriel Moreau (CNRS / LEGI)
+################################################################
+
+# Since 2021/07
+# Obsolete script, now use swmb.ps1
+# Script for compatibility
+
+Write-Output "Warning: obsolete command, now use swmb.ps1"
+
+$cmd = (Get-PSCallStack)[0].ScriptName
+$cmd = ($cmd -replace 'Win10.ps1$', 'swmb.ps1') + ' ' + $args
+
+Invoke-Expression -Command $cmd

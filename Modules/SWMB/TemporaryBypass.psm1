59d9244c3e44fda62895a3a83e81fd2bf4262bca
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2021-09-10 10:20:43 +0200

    Add DisableMSHTMLActiveX (CVE-2021-40444) mitigation

--- Modules/SWMB/TemporaryBypass.psm1
+++ Modules/SWMB/TemporaryBypass.psm1
@@ -15,6 +15,7 @@
 # 2021/07/05
 # https://www.cert.ssi.gouv.fr/alerte/CERTFR-2021-ALE-013/
 # CVE-2021-34527 exploit to keep your Print Servers running while a patch is not available
+# Disable
 Function DisablePrintForSystem {
 	Write-Output "Disable print for System user (CVE-2021-34527)..."
 	$acl = Get-Acl -Path "$Env:SystemRoot\System32\spool\drivers"
@@ -29,6 +30,7 @@ Function DisablePrintForSystem {
 	$acl | Set-Acl -Path "$Env:SystemRoot\System32\spool\drivers"
 }
 
+# Enable
 Function EnablePrintForSystem {
 	Write-Output "Enable print for System user (CVE-2021-34527)..."
 	$acl = Get-Acl -Path "$Env:SystemRoot\System32\spool\drivers"
@@ -43,8 +45,44 @@ Function EnablePrintForSystem {
 	$acl | Set-Acl -Path "$Env:SystemRoot\System32\spool\drivers"
 }
 
+# View
 Function ViewPrintForSystem {
 	Get-Acl -Path "$Env:SystemRoot\System32\spool\drivers" | Select -Expand Access | Out-GridView
+}
+
+################################################################
+
+# 2021/09/10
+# http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-40444
+# https://msrc.microsoft.com/update-guide/vulnerability/CVE-2021-40444
+# Microsoft MSHTML Remote Code Execution Vulnerability
+Function DisableMSHTMLActiveX {
+	Write-Output "Disable ActiveX in MSHTML (CVE-2021-40444)..."
+	For ($zone = 0 ; $zone -le 3 ; $zone++){
+		If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\$zone")) {
+			New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\$zone" -Force | Out-Null
+		}
+		Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\$zone" -Name "1001" -Type DWord -Value 00000003
+		Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\$zone" -Name "1004" -Type DWord -Value 00000003
+	}
+}
+
+# Enable
+Function EnableMSHTMLActiveX {
+	Write-Output "Enable ActiveX in MSHTML (CVE-2021-40444)..."
+	For ($zone = 0 ; $zone -le 3 ; $zone++){
+		Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\$zone" -Name "1001" -ErrorAction SilentlyContinue
+		Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\$zone" -Name "1004" -ErrorAction SilentlyContinue
+	}
+}
+
+# View
+Function ViewMSHTMLActiveX {
+	Write-Output 'ActiveX in MSHTML (not exist - enable, 3 disable)'
+	For ($zone = 0 ; $zone -le 3 ; $zone++){
+		Get-ItemProperty -Path -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\$zone" -Name "1001"
+		Get-ItemProperty -Path -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\$zone" -Name "1004"
+	}
 }
 
 

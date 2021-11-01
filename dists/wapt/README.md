fc4c75ca3507f34df530c97904993c0c55d7f21d
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2021-11-01 14:36:40 +0100

    Add WAPT package and continuous integration for build zip wapt folder

---
+++ dists/wapt/README.md
@@ -0,0 +1,7 @@
+# SWMB - Secure Windows Mode Batch
+
+source : https://resinfo-gt.pages.in2p3.fr/swmb/resinfo-swmb/
+
+maintainer : <Gabriel.Moreau at legi.grenoble-inp.fr>
+
+date : __DATE__
---
+++ dists/wapt/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# ----------------------------------------------------------------------
+#
+# This file is part of project CNRS RESINFO SWMB
+# Copyright (c) 2017-2020, Disassembler <disassembler@dasm.cz>
+# Copyright (C) 2020-2021, CNRS, France
+#
+# License: MIT License (Same as project Win10-Initial-Setup-Script)
+# Homepage: https://gitlab.in2p3.fr/resinfo-gt/swmb/resinfo-swmb
+#
+# SWMB is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MIT License for more details.
+#
+# Authors:
+#  2021 - Gabriel Moreau (CNRS / LEGI)
+#
+# ----------------------------------------------------------------------
+
+from setuphelpers import *
+
+uninstallkey = []
+
+setup_path = makepath(programfiles64,'SWMB')
+
+def install():
+	version = control.version.split('-',1)[0]
+	install_exe_if_needed("SWMB-Setup-%s.exe" % version, '/S')
+
+def uninstall():
+	run(r'"%s" /S' % makepath(setup_path, r'Uninst.exe'))

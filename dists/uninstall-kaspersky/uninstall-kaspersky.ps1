74783a5fea96a0ab0c3f3805a24c17d978ea3913
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-04-28 11:25:38 +0200

    Add custom

--- dists/uninstall-kaspersky/uninstall-kaspersky.ps1
+++ dists/uninstall-kaspersky/uninstall-kaspersky.ps1
@@ -1,2 +1,9 @@
 
-"C:\Program Files\SWMB\swmb.ps1" -exp UninstallKasperskyEndpoint
+If (!(Test-Path -LiteralPath ".\Custom-VarOverload.psm1")) {
+	Write-Output "You must have a Custom-VarOverload.psm1 configuration"
+	Write-Output "file in the current folder to define credentials for"
+	Write-Output "uninstalling Kaspersky Endpoint software."
+	Exit
+}
+
+"${Env:ProgramFiles}\SWMB\swmb.ps1" -exp -import "${Env:ProgramFiles}\SWMB\Modules\SWMB\Custom.psm1" UninstallKasperskyEndpoint

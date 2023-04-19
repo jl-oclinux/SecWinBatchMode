15cfac315043e6ef5f60d0967119d967fe1cf3fb
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-04-20 00:19:08 +0200

    Better comment and forget two files !

--- dists/swln/uninstall.bat
+++ dists/swln/uninstall.bat
@@ -2,13 +2,15 @@ ECHO OFF
 
 SET regkey=SWLN
 
-REM Cleean ProgramData
+REM Clean the ProgramData folder
 DEL /F /Q "%ProgramData%\SWMB\Presets\CurrentUser-Logon.preset"
 DEL /F /Q "%ProgramData%\SWMB\Presets\LocalMachine-Boot.preset"
+DEL /F /Q "%ProgramData%\SWMB\Presets\LocalMachine-PostInstall.preset"
 DEL /F /Q "%ProgramData%\SWMB\Modules\Custom-VarOverload.psm1"
+DEL /F /Q "%ProgramData%\SWMB\Modules\Local-Addon.psm1"
 
-REM Clean registry
+REM Clean the registry
 reg delete HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\%regkey% /f
 
-REM Clean the SWLN folder
+REM Clean the SWLN folder (must be the last line - auto-remove)
 IF EXIST "%ProgramFiles%\SWLN" RMDIR /S /Q "%ProgramFiles%\SWLN"

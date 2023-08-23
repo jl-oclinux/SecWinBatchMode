60e8c145d124eee819493ed101dcdf53c1d924ca
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-08-23 23:34:48 +0200

    Very simple powershell client

---
+++ dists/webhook/client.ps1
@@ -0,0 +1,12 @@
+#!/usr/bin/env pwsh
+
+$Url='http://localhost:9000/hooks/swmh?token=432&status=logon'
+$Payload='{"token": 432, "swmb": {"hostname":"abcmywin", "hostid":"fjjkfhjkj", "osversion": "10.0.45", "username":"toto", "isadmin": false, "version":"3.6.9"}}' 
+
+# Invoke-RestMethod -Uri $Url -Body ($Payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json' | Out-File -FilePath ./CurrentUser-Logon-Webhook.preset
+
+$Preset=$(Invoke-RestMethod -Uri $Url -Body "$Payload" -Method Post -ContentType 'application/json')
+
+$Preset
+
+# Invoke-RestMethod -Uri $Url -Body "$Payload" -Method Post -ContentType 'application/json' | Out-File -FilePath ./CurrentUser-Logon-Webhook.preset

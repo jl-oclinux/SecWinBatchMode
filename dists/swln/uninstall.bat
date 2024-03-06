95c60f597cd0cea0d627e6c09f0caa1bab4fc74e
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-03-06 13:38:18 +0100

    Uninstall script is now more generic

--- dists/swln/uninstall.bat
+++ dists/swln/uninstall.bat
@@ -1,16 +1,17 @@
 ECHO OFF
 
-SET softregkey=SWLN
+SET softname=__SWLN_NAME__
+SET softregkey=%softname%
 
-REM Clean the ProgramData folder
+ECHO Clean the ProgramData folder
 DEL /F /Q "%ProgramData%\SWMB\Presets\CurrentUser-Logon.preset"
 DEL /F /Q "%ProgramData%\SWMB\Presets\LocalMachine-Boot.preset"
 DEL /F /Q "%ProgramData%\SWMB\Presets\LocalMachine-PostInstall.preset"
 DEL /F /Q "%ProgramData%\SWMB\Modules\Custom-VarOverload.psm1"
 DEL /F /Q "%ProgramData%\SWMB\Modules\Local-Addon.psm1"
 
-REM Clean the registry
+ECHO Clean the registry
 reg delete HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\%softregkey% /f
 
-REM Clean the SWLN folder (must be the last line - auto-remove)
-IF EXIST "%ProgramFiles%\SWLN" RMDIR /S /Q "%ProgramFiles%\SWLN"
+ECHO Clean the %softname% installation folder (must be the last line - auto-remove)
+IF EXIST "%ProgramFiles%\%softname%" RMDIR /S /Q "%ProgramFiles%\%softname%"

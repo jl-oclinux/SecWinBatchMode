95d28a76ca0dec4b089a47d3e27ffb565ad8a109
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-10-01 16:34:48 +0200

    Try mode in task

--- Tasks/LocalMachine-Boot.ps1
+++ Tasks/LocalMachine-Boot.ps1
@@ -12,15 +12,25 @@
 # Version: v3.13, 2021-11-22
 ################################################################
 
-Write-EventLog -LogName Application -Source "SWMB" -EntryType Information -EventID 0 `
-	-Message "SWMB: Run Boot Script for LocalMachine ${Env:ComputerName} - Begin"
+Param (
+	# running mode: batch, print or check
+	[string]$Mode = 'batch'
+)
+
+If ($Mode -eq 'batch') {
+	Write-EventLog -LogName Application -Source "SWMB" -EntryType Information -EventID 0 `
+		-Message "SWMB: Run Boot Script for LocalMachine ${Env:ComputerName} - Begin"
+}
 
 # Change Path to the root Installation Folder
+$BeforeLocation = Get-Location
 $InstallFolder = (Join-Path -Path ${Env:ProgramFiles} -ChildPath "SWMB")
 If (Test-Path "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\SWMB") {
 	$InstallFolder = (Get-ItemProperty -Path "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\SWMB" -Name "InstallFolder").InstallFolder
 }
-Set-Location $InstallFolder
+If (Test-Path -LiteralPath $InstallFolder) {
+	Set-Location $InstallFolder
+}
 
 # Define SWMB folder on ProgramData
 $DataFolder   = (Join-Path -Path ${Env:ProgramData} -ChildPath "SWMB")
@@ -34,8 +44,15 @@ $HostExt="Host-$(${Env:ComputerName}.ToLower())"
 $Args = @()
 $FlagPreset = $False
 
-# Log action
-$Args += '-log', "$LogFolder\LocalMachine-LastBoot.log"
+# mode print
+If ($Mode -eq 'batch') {
+	# Log action
+	$Args += '-log', "$LogFolder\LocalMachine-LastBoot.log"
+} ElseIf ($Mode -eq 'print') {
+	$Args += '-print'
+} ElseIf ($Mode -eq 'check') {
+	$Args += '-check'
+}
 
 # Site and Host Modules
 If (Test-Path -LiteralPath "$ModuleFolder\Local-Addon.psm1") {
@@ -58,8 +75,10 @@ If (Test-Path -LiteralPath "$PresetFolder\LocalMachine-Boot-$HostExt.preset") {
 	$FlagPreset = $True
 }
 
-# Hash checkpoint
-$Args += '-hash', "$CacheFolder\LocalMachine-LastBoot.hash"
+If ($Mode -eq 'batch') {
+	# Hash checkpoint
+	$Args += '-hash', "$CacheFolder\LocalMachine-LastBoot.hash"
+}
 
 # Launch SWMB with this preset
 If ($FlagPreset) {
@@ -68,5 +87,11 @@ If ($FlagPreset) {
 	Write-Output "Error: No preset define, No SWMB launch"
 }
 
-Write-EventLog -LogName Application -Source "SWMB" -EntryType Information -EventID 999 `
-	-Message "SWMB: Run Boot Script for LocalMachine ${Env:ComputerName} - End"
+If (Test-Path -LiteralPath $BeforeLocation) {
+	Set-Location $BeforeLocation
+}
+
+If ($Mode -eq 'batch') {
+	Write-EventLog -LogName Application -Source "SWMB" -EntryType Information -EventID 999 `
+		-Message "SWMB: Run Boot Script for LocalMachine ${Env:ComputerName} - End"
+}

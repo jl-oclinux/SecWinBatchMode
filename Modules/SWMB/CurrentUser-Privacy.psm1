a8bb9532bf7d8ce7999d4174af40abb5791520aa
Olivier De Marchi <olivier.de-marchi@univ-grenoble-alpes.fr>
2025-01-27 15:33:49 +0100

    closes #62 see function TweakDisableFeedback in LocalMachine-Privacy module

--- Modules/SWMB/CurrentUser-Privacy.psm1
+++ Modules/SWMB/CurrentUser-Privacy.psm1
@@ -115,8 +115,6 @@ Function TweakDisableFeedback_CU {
 		New-Item -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Force | Out-Null
 	}
 	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Name "NumberOfSIUFInPeriod" -Type DWord -Value 0
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "DoNotShowFeedbackNotifications" -Type DWord -Value 1
-	# HKCU or HKLM ?
 	Disable-ScheduledTask -TaskName "Microsoft\Windows\Feedback\Siuf\DmClient" -ErrorAction SilentlyContinue | Out-Null
 	Disable-ScheduledTask -TaskName "Microsoft\Windows\Feedback\Siuf\DmClientOnScenarioDownload" -ErrorAction SilentlyContinue | Out-Null
 }
